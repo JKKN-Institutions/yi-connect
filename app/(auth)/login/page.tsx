18dee19ac1b2190c5ec17f3e5c8ac0666127d813
Boobal <a.boobalzen003@gmail.com>
2025-11-10 16:32:16 +0530

    Update page.tsx

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -2,8 +2,29 @@
  * Login Page
  */
 
+import { Suspense } from 'react'
 import { LoginForm } from '@/components/auth/login-form'
+import { Skeleton } from '@/components/ui/skeleton'
+
+// Loading fallback for LoginForm
+function LoginFormSkeleton() {
+  return (
+    <div className='space-y-6'>
+      <div className='space-y-2 text-center'>
+        <Skeleton className='h-9 w-64 mx-auto' />
+        <Skeleton className='h-5 w-96 mx-auto' />
+      </div>
+      <Skeleton className='h-12 w-full' />
+      <Skeleton className='h-20 w-full' />
+      <Skeleton className='h-10 w-full' />
+    </div>
+  )
+}
 
 export default function LoginPage() {
-  return <LoginForm />
+  return (
+    <Suspense fallback={<LoginFormSkeleton />}>
+      <LoginForm />
+    </Suspense>
+  )
 }
